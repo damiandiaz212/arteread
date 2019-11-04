7d950292de1c7258321ff29aadcaa6722976c82d
Damian <damiandiaz2025@gmail.com>
2019-11-04 04:50:09 -0500

    began work on the restful flask service, changes to ingestion

--- server/app.py
+++ server/app.py
@@ -1,33 +1,17 @@
 #!flask/bin/python
 from flask import Flask, jsonify
+from ingestion import *
 
 app = Flask(__name__)
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
-    }
-]
+@app.route('/api/v1.0/search/<string:keywords>/api=<string:api>', methods=['POST'])
+def get_info(keywords, api):
 
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
-def get_tasks():
-    return jsonify({'tasks': tasks})
-
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
-def get_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    return jsonify({'task': task[0]})
+    ingest = ingestion()
+    val = ingest.getInfoJSON(keywords,api)
+    return val
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
--- server/ingestion.py
+++ server/ingestion.py
@@ -3,100 +3,111 @@ import json
 import timeit
 import sys
 
-API = sys.argv[1]
-URL_1 = 'https://api.nal.usda.gov/fdc/v1/search'
-URL_2 = 'https://api.nal.usda.gov/fdc/v1/'
+class ingestion:
 
-headers = {'Content-Type':'application/json'}
-auth = (API, '')
+	def getInfoJSON(self, keywords, api):
 
-# what we return if we have a bad request
-error = {
-	"message":"error"
-}
+		URL_1 = 'https://api.nal.usda.gov/fdc/v1/search'
+		URL_2 = 'https://api.nal.usda.gov/fdc/v1/'
 
-def getInfo(keywords):
-	
-	# create payload
-	payload = {"generalSearchInput":keywords}
-	
-	# get food code
-	start = timeit.default_timer()
-	response = requests.post(URL_1, data=json.dumps(payload), headers=headers, auth=auth)
-	stop = timeit.default_timer()
-	time = stop - start
+		headers = {'Content-Type':'application/json'}
 
-	if response.status_code == 200:
-	
-		response = response.json()
+		# what we return if we have a bad request
+		error = {
+			"message":"error"
+		}
 
-		# grab the food code from the top result
-		foods = response['foods']
-		top_result = foods[0]
-		food_code = str(top_result['fdcId'])
+		auth = (api, '')
+		
+		# create payload
+		payload = {"generalSearchInput":keywords}
+		
+		# get food code
+		start = timeit.default_timer()
+		response = requests.post(URL_1, data=json.dumps(payload), headers=headers, auth=auth)
+		stop = timeit.default_timer()
+		time = stop - start
 
-		# debug
-		print('Food Code Retrieved: ' + food_code + ' took(sec): ', time)
-	
-	else:
-		print('[GENERAL SEARCH REQUEST FAILED]')
-		print(response.request.url)
-		print(response.status_code)
-		print(response.json())
-		return error
+		if response.status_code == 200:
+		
+			response = response.json()
 
-	#######
+			# grab the food code from the top result
+			foods = response['foods']
+			top_result = foods[0]
+			food_code = str(top_result['fdcId'])
 
-	# get nutrient info
-	start = timeit.default_timer()
-	response = requests.get(URL_2+food_code, headers=headers, auth=auth)
-	stop = timeit.default_timer()
-	time = stop - start
-
-	if response.status_code == 200:
-
-		# debug
-		print('Food Data Retrieved: ' + food_code + ' took(sec): ', time)
-		response = response.json()
-
-		# parsing info
-		foodNutrients = response['foodNutrients']
-		nutrient_name = response['description']
-		nutrient_object  = findCholesterol(foodNutrients)
-
-		if nutrient_object == None:
-			print('Couldnt find Cholesterol content in' + nutrient_name)
+			# debug
+			print('Food Code Retrieved: ' + food_code + ' took(sec): ', time)
+		
+		else:
+			print('[GENERAL SEARCH REQUEST FAILED]')
+			print(response.request.url)
+			print(response.status_code)
+			print(response.json())
+			error['reason'] = 'unable to locate a result matching those keywords.'
 			return error
 
-		nutrient_amount = nutrient_object['amount']
-		nutrient_info = nutrient_object['nutrient']
-		printNutrientInfo(nutrient_name, nutrient_info, nutrient_amount)
-	else:
-		print('[CODE SEARCH REQUEST FAILED]')
-		print(response.request.url)
-		print(response.status_code)
-		print(response.json())
-		return error
+		#######
+
+		# get nutrient info
+		start = timeit.default_timer()
+		response = requests.get(URL_2+food_code, headers=headers, auth=auth)
+		stop = timeit.default_timer()
+		time = stop - start
+
+		if response.status_code == 200:
+
+			# debug
+			print('Food Data Retrieved: ' + food_code + ' took(sec): ', time)
+			response = response.json()
+
+			# parsing info
+			foodNutrients = response['foodNutrients']
+			nutrient_name = response['description']
+			nutrient_object  = self.findCholesterol(foodNutrients)
+
+			if nutrient_object == None:
+				print('Couldnt find Cholesterol content in' + nutrient_name)
+				error['reason'] = 'unable to find cholesterol.'
+				return error
+
+			nutrient_amount = nutrient_object['amount']
+			nutrient_info = nutrient_object['nutrient']
+			self.printNutrientInfo(nutrient_name, nutrient_info, nutrient_amount)
+			return self.getNutrientInfo(nutrient_name, nutrient_info, nutrient_amount)
+		else:
+			print('[CODE SEARCH REQUEST FAILED]')
+			print(response.request.url)
+			print(response.status_code)
+			print(response.json())
+			error['reason'] = 'fdcId invalid.'
+			return error
 
 
-def outputJSONData(data):
-	with open('data.txt', 'w') as out:
-		json.dump(data, out)
+	def outputJSONData(self, data):
+		with open('data.txt', 'w') as out:
+			json.dump(data, out)
 
-def printNutrientInfo(name, info, amount):
-	nName = info['name']
-	unit = info['unitName']
-	print('\nFood: ' + name + '\n' + nName +': ' + str(amount) + ' ' + unit)
+	def printNutrientInfo(self, name, info, amount):
+		nName = info['name']
+		unit = info['unitName']
+		print('\nFood: ' + name + '\n' + nName +': ' + str(amount) + ' ' + unit)
 
-def findCholesterol(data):
+	def getNutrientInfo(self, name, info, amount):
+		nName = info['name']
+		unit = info['unitName']
+		return {'Food':name, 'Type':nName, 'Unit':unit, 'Amount':amount}
 
-	for i in data:
-		temp = i['nutrient']
-		if temp['name'] == 'Cholesterol':
-			return i
+	def findCholesterol(self, data):
 
-	return None
+		for i in data:
+			temp = i['nutrient']
+			if temp['name'] == 'Cholesterol':
+				return i
+
+		return None
 
 
 
-getInfo(str(sys.argv[2]))
+	#getInfo(sys.argv[1], str(sys.argv[2]))
